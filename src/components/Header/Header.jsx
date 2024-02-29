80d37389c0cfa269087de27fb37738ca81b58de3
Lucas Rocha <lucasbezerrar@gmail.com>
2024-02-29 12:22:53 -0300

    Identação

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -5,11 +5,10 @@ export const Header = () => {
     <div className='header'>
       <h1>Rick and Morty</h1>
       <div>
-      <ul>
-        <li><a href="#">Home</a></li>
-        <li><a href="#">Sobre</a></li>
-      </ul>
-
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Sobre</a></li>
+        </ul>
       </div>
     </div>
   )
