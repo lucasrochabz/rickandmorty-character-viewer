05011aab2fc6a3691d1318a59d9118c0c68d675c
lucasrochabz <lucasbezerrar@gmail.com>
2024-07-13 16:20:21 -0300

    Refatorando

--- src/App.jsx
+++ src/App.jsx
@@ -5,58 +5,65 @@ import { Footer } from './components/Footer/Footer';
 import './App.css';
 
 export const App = () => {
-  const [characters, setCharacters] = useState([])
-  const [currentPage, setCurrentPage] = useState(1)
-  const [totalPages, setTotalPages] = useState(1)
-  const [loading, setLoading] = useState(false)
-  
+  const [characters, setCharacters] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [totalPages, setTotalPages] = useState(1);
+  const [loading, setLoading] = useState(false);
+
   const loadCharacters = async (page = 1) => {
     try {
-      setLoading(true)
-      const response = await fetch(`https://rickandmortyapi.com/api/character/?page=${page}`)
-      const data = await response.json()
-      const loadedCharacters = data.results
+      setLoading(true);
+      const response = await fetch(
+        `https://rickandmortyapi.com/api/character/?page=${page}`,
+      );
+      const data = await response.json();
+      const loadedCharacters = data.results;
 
-      setCharacters(loadedCharacters)
-      setCurrentPage(page)
-      setTotalPages(data.info.pages)
+      setCharacters(loadedCharacters);
+      setCurrentPage(page);
+      setTotalPages(data.info.pages);
     } catch (error) {
     } finally {
-      setLoading(false)
+      setLoading(false);
     }
-  }
+  };
 
   const handleNextPage = async () => {
-    if(currentPage < totalPages) {
-      const nextPage = currentPage + 1
-      await loadCharacters(nextPage)
+    if (currentPage < totalPages) {
+      const nextPage = currentPage + 1;
+      await loadCharacters(nextPage);
     }
-  }
+  };
 
   const handlePrevPage = async () => {
-    if(currentPage > 1) {
-      const prevPage = currentPage - 1
-      await loadCharacters(prevPage)
+    if (currentPage > 1) {
+      const prevPage = currentPage - 1;
+      await loadCharacters(prevPage);
     }
-  }
+  };
 
   useEffect(() => {
-    loadCharacters()
-  }, [])
+    loadCharacters();
+  }, []);
 
-  return(
+  return (
     <>
       <Header />
       {loading && <p>Carregando...</p>}
       {!loading && (
         <>
-          <CharacterList characters={characters}/>
+          <CharacterList characters={characters} />
           <div>
             <button onClick={handlePrevPage} disabled={currentPage === 1}>
               Página Anterior
             </button>
-            <span>Página {currentPage} de {totalPages}</span>
-            <button onClick={handleNextPage} disabled={currentPage === totalPages}>
+            <span>
+              Página {currentPage} de {totalPages}
+            </span>
+            <button
+              onClick={handleNextPage}
+              disabled={currentPage === totalPages}
+            >
               Próxima Página
             </button>
           </div>
@@ -64,5 +71,5 @@ export const App = () => {
       )}
       <Footer />
     </>
-  )
-}
+  );
+};
